05abe74517f967e709fb722848c0de3de66307e2
Yarin <yarin.cardillo@icloud.com>
2022-10-19 20:06:09 +0200

    pointers

--- test/main.c
+++ test/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include "stdlib.h"
+#include "string.h"
 
 int main() {
-    printf("Hello, World!\n");
+    typedef struct Studente{
+        char nome[20];
+        char cognome[20];
+        int matricola;
+    }studente;
+
+    studente *jo=(studente *) malloc(sizeof(studente));
+
+    strcpy(jo->nome, "Giovanni");
+    strcpy(jo->cognome, "Crimaldi");
+    jo->matricola = 123456;
+
+    fprintf(stdout, "Nome: %s\n"
+                    "Cognome: %s\n"
+                    "Matricola: %d\n", jo->nome, jo->cognome, jo->matricola);
+
     return 0;
 }
